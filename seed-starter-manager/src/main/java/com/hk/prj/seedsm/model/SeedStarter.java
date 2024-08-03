a7fd58ed5e114c6456579a7c84e5b1e8348e38c1
hkcodebase <hkchauhan022@gmail.com>
2024-08-03 09:18:08 -0400

    Revert "version upgraded to java 21 and springboot 3.3"
    This reverts commit 38bd99cb6d9c56a704c2e2d4a18c2beb0388d98c.

--- seed-starter-manager/src/main/java/com/hk/prj/seedsm/model/SeedStarter.java
+++ seed-starter-manager/src/main/java/com/hk/prj/seedsm/model/SeedStarter.java
@@ -5,9 +5,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 
