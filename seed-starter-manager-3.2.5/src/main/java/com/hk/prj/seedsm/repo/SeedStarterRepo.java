f135dd8502c30c2e910fb5859640a869557febd5
hkcodebase <hkchauhan022@gmail.com>
2024-08-03 09:19:40 -0400

    old version of seed starter manager added

--- seed-starter-manager-3.2.5/src/main/java/com/hk/prj/seedsm/repo/SeedStarterRepo.java
+++
@@ -1,9 +0,0 @@
-package com.hk.prj.seedsm.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hk.prj.seedsm.model.SeedStarter;
-
-public interface SeedStarterRepo extends JpaRepository<SeedStarter, Long> {
-
-}
